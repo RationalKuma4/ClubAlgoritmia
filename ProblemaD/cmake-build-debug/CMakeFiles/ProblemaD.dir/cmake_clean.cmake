224178d85c73a1b554c43465966bc60d3f74cca0
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-04-09 19:23:50 -0500

    Propuesta solucion problema d

---
+++ ProblemaD/cmake-build-debug/CMakeFiles/ProblemaD.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProblemaD.dir/main.cpp.obj"
+  "ProblemaD.pdb"
+  "ProblemaD.exe"
+  "ProblemaD.exe.manifest"
+  "libProblemaD.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProblemaD.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ProblemaD/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int t = 0;
+    cin >> t;
+    if (t >= 15) return 0;
+    int a, b;
+    for (int i = 0; i < t; ++i)
+    {
+        cin >> a >> b;
+        if (abs(a) < abs(b)) cout << "<" << "\n";
+        else if (abs(a) > abs(b)) cout << ">" << "\n";
+        else cout << "=" << "\n";
+    }
+    return 0;
+}
