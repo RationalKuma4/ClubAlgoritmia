b45d5854d5f902a9eee3f3b902573c23d3721bb2
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-04-11 17:26:39 -0500

    Problema E aceptado

--- ProblemaE/main.cpp
+++ ProblemaE/main.cpp
@@ -4,15 +4,27 @@ using namespace std;
 
 int main()
 {
-    int k = 0, n, m, x, y;
-    cin >> k;
-
-    for (int i = 0; i < k; ++i)
+    int k = 0, n = 0, m = 0, x = 0, y = 0;
+    do
     {
+        cin >> k;
+        if (k == 0) break;
         cin >> n >> m;
-        cin >> x >> y;
 
-    }
-
+        for (int i = 0; i < k; ++i)
+        {
+            cin >> x >> y;
+            if (n == x || m == y)
+                cout << "divisa" << "\n";
+            else if (x > n && y > m)
+                cout << "NE" << "\n";
+            else if (x < n && y > m)
+                cout << "NO" << "\n";
+            else if (x < n && y < m)
+                cout << "SO" << "\n";
+            else
+                cout << "SE" << "\n";
+        }
+    } while (k != 0);
     return 0;
 }
