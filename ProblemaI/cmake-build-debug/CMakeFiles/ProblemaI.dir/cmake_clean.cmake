89c5483d13d4a873a68d991bd5fd2fdf198279d7
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-04-12 12:05:49 -0500

    ProblmaI Parcial

---
+++ ProblemaI/cmake-build-debug/CMakeFiles/ProblemaI.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProblemaI.dir/main.cpp.obj"
+  "ProblemaI.pdb"
+  "ProblemaI.exe"
+  "ProblemaI.exe.manifest"
+  "libProblemaI.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProblemaI.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ProblemaI/main.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+struct Hotel
+{
+    int PrecioPersona;
+    vector<int> CamasDisponiblesFin;
+
+    Hotel(int precioPersona)
+    {
+        PrecioPersona = precioPersona;
+    }
+};
+
+int main()
+{
+    int n, b, h, w;
+    cin >> n >> b >> h >> w;
+    vector<Hotel> hoteles;
+    vector<int> preciosFinales;
+    int precioFinal = 0;
+
+    for (int i = 0; i < h; ++i)
+    {
+        int precio;
+        cin >> precio;
+        Hotel nuevoHotel = Hotel(precio);
+
+        for (int j = 0; j < w; ++j)
+        {
+            int camasDisponibles;
+            cin >> camasDisponibles;
+            nuevoHotel.CamasDisponiblesFin.push_back(camasDisponibles);
+        }
+        hoteles.push_back(nuevoHotel);
+    }
+
+    for (Hotel hotel:hoteles)
+    {
+        if (b < hotel.PrecioPersona * n) continue;
+        for (int camasDisponibles: hotel.CamasDisponiblesFin)
+        {
+            if (n > camasDisponibles) continue;
+            else preciosFinales.push_back(n * hotel.PrecioPersona);
+        }
+    }
+
+    if (precioFinal > 0)
+    {
+        precioFinal = preciosFinales[0];
+        for (int k = 1; k < preciosFinales.size(); ++k)
+            precioFinal = min(precioFinal, preciosFinales[k]);
+    }
+
+    if (precioFinal > 0) cout << precioFinal << "\n";
+    else cout << "stay home" << "\n";
+    return 0;
+}
