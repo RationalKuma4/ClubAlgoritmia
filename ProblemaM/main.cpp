5a0355a71c9c01cca5035435ca0b26e29c1a1ddb
Anselmo Alvarado <RationalKuma4@users.noreply.github.com>
2018-04-13 22:19:59 -0500

    Correcion

--- ProblemaM/main.cpp
+++ ProblemaM/main.cpp
@@ -1,31 +1,33 @@
 #include <iostream>
 #include <set>
-
-#define numIn 300
-
+#define lista 1000
 using namespace std;
 
-int main()
-{
-    set<int> jolly;
-    long long int n, dif, cont, num[numIn];
-    cont = 0;
-    while (cin >> n)
-    {
-        for (int i = 0; i <= n - 1; i++)
-            cin >> num[i];
+long long int absolute(int a){
+    if(a<0){
+        return -a;
+    }else return a;
+}
 
-        for (int i = 0; i <= n - 2; i++)
-        {
-            dif = num[i] - num[i + 1];
-            jolly.insert(abs(dif));
+int main(){
+    set <long long int> jolly;
+    long long int n, dif, cont, num[lista];
+    cont=0;
+    while(cin>>n){
+        for(int i=0; i<=n-1; i++){
+            cin>>num[i];
         }
-
-        for (int i = 1; i <= n - 1; i++)
-            if (jolly.count(i))cont++;
-
-        if (cont == n - 1) cout << "Jolly" << endl;
-        else cout << "Not Jolly" << endl;
+        for(int i=0; i<=n-2; i++){
+            dif=num[i]-num[i+1];
+            jolly.insert(absolute(dif));
+        }
+        for(int i=1; i<=n-1; i++){
+            if(jolly.count(i)>0){
+                cont++;
+            }
+        }
+        if(cont==n-1) cout<<"Jolly"<<endl;
+        else cout<<"Not Jolly"<<endl;
     }
     return 0;
 }
